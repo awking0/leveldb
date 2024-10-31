fdf82fb3a77b79091241778b77d1343be29bea61
aaron <aaron.king@orevillestudios.com>
2024-10-31 04:52:53 -0400

    Compile + Java 9 fixes

--- leveldb/src/main/java/org/iq80/leveldb/util/ByteBufferSupport.java
+++ leveldb/src/main/java/org/iq80/leveldb/util/ByteBufferSupport.java
@@ -18,25 +18,53 @@
 package org.iq80.leveldb.util;
 
 import com.google.common.base.Throwables;
-import sun.nio.ch.FileChannelImpl;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 
 public final class ByteBufferSupport
 {
-    private static final Method unmap;
+    private static final MethodHandle INVOKE_CLEANER;
 
     static {
-        Method x;
+        MethodHandle invoker;
         try {
-            x = FileChannelImpl.class.getDeclaredMethod("unmap", MappedByteBuffer.class);
+            // Java 9 added an invokeCleaner method to Unsafe to work around
+            // module visibility issues for code that used to rely on DirectByteBuffer's cleaner()
+            Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field theUnsafe = unsafeClass.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            invoker = MethodHandles.lookup()
+                    .findVirtual(unsafeClass, "invokeCleaner", MethodType.methodType(void.class, ByteBuffer.class))
+                    .bindTo(theUnsafe.get(null));
         }
-        catch (NoSuchMethodException e) {
-            throw new AssertionError(e);
+        catch (Exception e) {
+            // fall back to pre-java 9 compatible behavior
+            try {
+                Class<?> directByteBufferClass = Class.forName("java.nio.DirectByteBuffer");
+                Class<?> cleanerClass = Class.forName("sun.misc.Cleaner");
+
+                Method cleanerMethod = directByteBufferClass.getDeclaredMethod("cleaner");
+                cleanerMethod.setAccessible(true);
+                MethodHandle getCleaner = MethodHandles.lookup().unreflect(cleanerMethod);
+
+                Method cleanMethod = cleanerClass.getDeclaredMethod("clean");
+                cleanerMethod.setAccessible(true);
+                MethodHandle clean = MethodHandles.lookup().unreflect(cleanMethod);
+
+                clean = MethodHandles.dropArguments(clean, 1, directByteBufferClass);
+                invoker = MethodHandles.foldArguments(clean, getCleaner);
+            }
+            catch (Exception e1) {
+                throw new AssertionError(e1);
+            }
         }
-        x.setAccessible(true);
-        unmap = x;
+        INVOKE_CLEANER = invoker;
     }
 
     private ByteBufferSupport()
@@ -46,9 +74,9 @@ public final class ByteBufferSupport
     public static void unmap(MappedByteBuffer buffer)
     {
         try {
-            unmap.invoke(null, buffer);
+            INVOKE_CLEANER.invoke(buffer);
         }
-        catch (Exception ignored) {
+        catch (Throwable ignored) {
             throw Throwables.propagate(ignored);
         }
     }
